7ef744a94d90c36c494395bdc10da225300f3a3c
mukhyiddin <yudivandiego@gmail.com>
2022-02-11 15:30:10 +0800

    Done

---
+++ scripts.js
@@ -0,0 +1,33 @@
+/// Soal - 01
+/// Kamu diminta untuk menjelaskan apa perbedaan antara
+/// - Type Error
+/// - Reference Error
+/// - Range Error
+/// - Syntax Error
+
+console.log("--------------SOAL 01----------------------------")
+console.log("Type Error merupakan saat mengubah value variabel yang dimana type datanya berbeda contohnya mengubah 1 menjadi huruf besar");
+console.log("Reference Error merupakan error yang dimana variabel di panggil sebelum di deklarasikan terlebih dahulu");
+console.log("Range Error merupakan error ketika angka tertentu dimasukan diatas nilai maximum");
+console.log("Syntax Error merupakan error dari kesalahan code pada syntax tertentu");
+/// WRITE YOUR ANALYSIS HERE
+
+
+
+/// Soal - 02
+/// Kamu diminta untuk melakukan analisa terhadap baris kode dibawah dengan instruksi detail sebagai berikut:
+/// - apabila kita menjalankan baris kode dibawah apa yang akan terjadi?
+/// - apabila terjadi error, termasuk dalam kategori manakah error tersebut?
+/// - apabila terjadi error, jelaskan mengapa hal tersebut bisa terjadi
+
+/// WRITE YOUR ANALYSIS HERE
+
+console.log("--------------SOAL 02----------------------------")
+console.log("Error di bawah ini termasuk dalam Reference error")
+console.log("Dikaeranakan variabel blum di deklarasikan terlebih dahulu sebelum di panggil");
+
+console.log(salaryWithVar)
+console.log(salaryWithConst)
+
+var salaryWithVar = 15000000;
+const salaryWithConst = 15000000;
